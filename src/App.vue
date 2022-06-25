9907740bcacbd71698c0e8ab09bd61a0ea000e45
shiotomo <subtle.tomo@gmail.com>
2022-06-26 02:08:37 +0900

    feat: Add HelloWorld.vue.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <div class="w-screen h-screen">
+    <HelloWorld />
+  </div>
+</template>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full h-full bg-gray-100 dark:bg-gray-900">
+    <div class="flex justify-center items-center h-full">
+      <h1 class="text-4xl text-center text-gray-600 dark:text-gray-300">
+        Shiotomo Dev Tools
+      </h1>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
