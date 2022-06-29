5ec94bb81e8e259164bcbaacb82aa83733bf2d95
shiotomo <subtle.tomo@gmail.com>
2022-06-29 22:56:41 +0900

    refactor: refactor side bar menu.

--- src/components/side-bar/SideBarMenu.vue
+++ src/components/side-bar/SideBarMenu.vue
@@ -3,10 +3,9 @@
     <router-link
       class="flex items-center p-2 text-gray-700 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white hover:bg-gray-100 dark:hover:bg-gray-600 rounded-lg transition-colors duration-200"
       :to="item.to"
-      ><span class="mx-4 text-lg font-normal">{{
-        item.label
-      }}</span></router-link
     >
+      <span class="mx-4 text-lg font-normal">{{ item.label }} </span>
+    </router-link>
   </nav>
 </template>
 
