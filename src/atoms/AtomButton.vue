d81e9fc1f9e4b9edd278e65030fc39e8cefe507c
shiotomo <subtle.tomo@gmail.com>
2022-07-14 22:12:46 +0900

    feat: Update AtomButton to add diabled props.

--- src/atoms/AtomButton.vue
+++ src/atoms/AtomButton.vue
@@ -1,6 +1,8 @@
 <template>
   <button
     class="py-2 px-8 w-full text-lg text-white bg-indigo-500 hover:bg-indigo-600 rounded border-0 focus:outline-none"
+    :class="{ 'bg-gray-600 hover:bg-gray-600': disabled }"
+    :disabled="disabled"
     @click="handleClick"
   >
     {{ label }}
@@ -8,7 +10,10 @@
 </template>
 
 <script setup lang="ts">
-defineProps({ label: { type: String, default: 'Button' } });
+defineProps({
+  label: { type: String, default: 'Button' },
+  disabled: { type: Boolean, default: false },
+});
 
 const emit = defineEmits(['click']);
 
